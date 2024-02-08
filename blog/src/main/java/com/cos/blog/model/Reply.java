5cfc35fbf7f608802f788c40700a0bd09018d713
zhy2on <dhwlgus5940@gmail.com>
2024-02-08 16:14:40 +0900

    feat: Reply table 생성

--- blog/src/main/java/com/cos/blog/model/Reply.java
+++ blog/src/main/java/com/cos/blog/model/Reply.java
@@ -1,4 +1,36 @@
 package com.cos.blog.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity // ORM 클래스
 public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @ManyToOne // N:1 여러 개의 답변이 하나의 게시글에 존재할 수 있다. 답변이 N.
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    @ManyToOne // N:1 여러 개의 답변을 한 명의 유저가 쓸 수 있다. 답변이 N.
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
 }
