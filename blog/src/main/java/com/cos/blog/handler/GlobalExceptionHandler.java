fc4a063ea093b9d2138b4093f08f64460c887008
zhy2on <dhwlgus5940@gmail.com>
2024-02-17 14:20:40 +0900

    feat: GlobalExceptionHandler 추가

---
+++ blog/src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.cos.blog.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice // 어디서 Exception이 발생하든 여기로 오게 하기 위해서
+@RestController
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public String handleArgumentException(IllegalArgumentException e) {
+        return "<h1>" + e.getMessage() + "</h1>";
+    }
+}
---
+++ blog/src/main/java/com/cos/blog/repository/UserRepository.java
@@ -0,0 +1,2 @@
+package com.cos.blog.repository;public interface UserRepository {
+}
